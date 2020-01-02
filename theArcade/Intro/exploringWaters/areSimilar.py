ba471104b7182d7c4d48c397be54ccbbdef0c3d2
liamcclane <lia_mcclane@msn.com>
2020-01-01 20:59:25 -0800

    completed the first level of the core

--- theArcade/Intro/exploringWaters/areSimilar.py
+++ theArcade/Intro/exploringWaters/areSimilar.py
@@ -1,49 +1,105 @@
 # Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
-
-# Given two arrays a and b, check whether they are similar.
+    # Given two arrays a and b, check whether they are similar.
 
 # Example
+    # For a = [1, 2, 3] and b = [1, 2, 3], the output should be
+    # areSimilar(a, b) = true.
 
-# For a = [1, 2, 3] and b = [1, 2, 3], the output should be
-# areSimilar(a, b) = true.
+    # The arrays are equal, no need to swap any elements.
 
-# The arrays are equal, no need to swap any elements.
+    # For a = [1, 2, 3] and b = [2, 1, 3], the output should be
+    # areSimilar(a, b) = true.
 
-# For a = [1, 2, 3] and b = [2, 1, 3], the output should be
-# areSimilar(a, b) = true.
+    # We can obtain b from a by swapping 2 and 1 in b.
 
-# We can obtain b from a by swapping 2 and 1 in b.
+    # For a = [1, 2, 2] and b = [2, 1, 1], the output should be
+    # areSimilar(a, b) = false.
 
-# For a = [1, 2, 2] and b = [2, 1, 1], the output should be
-# areSimilar(a, b) = false.
-
-# Any swap of any two elements either in a or in b won't make a and b equal.
+    # Any swap of any two elements either in a or in b won't make a and b equal.
 
 # Input/Output
 
-# [execution time limit] 4 seconds (py3)
+    # [execution time limit] 4 seconds (py3)
 
-# [input] array.integer a
+    # [input] array.integer a
 
-# Array of integers.
+    # Array of integers.
 
-# Guaranteed constraints:
-# 3 ≤ a.length ≤ 105,
-# 1 ≤ a[i] ≤ 1000.
+    # Guaranteed constraints:
+    # 3 ≤ a.length ≤ 105,
+    # 1 ≤ a[i] ≤ 1000.
 
-# [input] array.integer b
+    # [input] array.integer b
 
-# Array of integers of the same length as a.
+    # Array of integers of the same length as a.
 
-# Guaranteed constraints:
-# b.length = a.length,
-# 1 ≤ b[i] ≤ 1000.
+    # Guaranteed constraints:
+    # b.length = a.length,
+    # 1 ≤ b[i] ≤ 1000.
 
-# [output] boolean
+    # [output] boolean
 
-# true if a and b are similar, false otherwise.
+    # true if a and b are similar, false otherwise.
 
 
 def areSimilarLia(a,b):
+
+    # do this check first since it is gaurenteed that the two arrays are of the same lenght
+    notMatchingCount = 0
+    for i in range(len(a)):
+        if a[i] is not b[i]:
+            notMatchingCount += 1
+
+    if notMatchingCount > 2:
+        return False
     
-print(areSimilarLia([1,2,3],[2,4,1]))
+    # this does quantity checking
+    dictA = {}
+    dictB = {}
+
+    # this does element checking and fills in the dictonaries
+    for val in a:
+        print(val, 'in a')
+        if val not in b:
+            return False
+        if val not in dictA:
+            dictA[val] = 1
+        else:
+            dictA[val] += 1
+    
+    for val in b:
+        print(val ,'in b')
+        if val not in a:
+            return False
+        if val not in dictB:
+            dictB[val] = 1
+        else:
+            dictB[val] += 1
+
+    print(dictA)
+    print(dictB)
+
+    # now we need to check if the matching keys in the dictonaries match
+    for key in dictA:
+        print("dictA[",key,"] is not dictB[",key,"]", dictA[key] ,":",dictB[key])
+        if dictA[key] is not dictB[key]:
+            return False
+
+    
+    return True
+
+# print(areSimilarLia([1,2,3],[2,4,1]),"line 84") # returns False
+# print(areSimilarLia([1,1,2],[2,2,1]),"line 85") # returns False
+# print(areSimilarLia([1,1,1,1,3],[3,3,3,3,1]),"line 86") # returns False
+# print(areSimilarLia([3,4,6],[4,6,3])) # returns False
+# print(areSimilarLia([3,4,6],[4,6,3])) # returns False
+print(areSimilarLia([832, 998, 148, 570, 533, 561, 894, 147, 455, 279],
+                    [832, 998, 148, 570, 533, 561, 455, 147, 894, 279])) # returns False
+
+# hi Toan can you see me live typing?
+# yes that is cool how can we comment to each other?
+
+# I can't run the code cuz I don't have the file
+
+# so I guess you can only really read and help me code what is on my screen
+# interresting..
--- theArcade/theCore/introGates.py
+++ theArcade/theCore/introGates.py
@@ -66,5 +66,174 @@ def candies(n, m):
 # For nCols = 16, nRows = 11, col = 5, and row = 3, the output should be
 # seatsInTheater(nCols, nRows, col, row) = 96.
 def seatsInTheater(nCols, nRows, col, row):
-    answer = 0
-    return answer
+    count = nRows - row
+
+    for i in range(nCols - col):
+        count += (nRows - row)
+
+    return count
+# print(seatsInTheater(16,11,5,3))
+# print(seatsInTheater(1,1,1,1))
+
+
+# ----MaxMultiple------
+# Given a divisor and a bound, find the largest integer N such that:
+
+# N is divisible by divisor.
+# N is less than or equal to bound.
+# N is greater than 0.
+# It is guaranteed that such a number exists.
+
+# Example:
+    # For divisor = 3 and bound = 10, the output should be
+    # maxMultiple(divisor, bound) = 9.
+
+    # The largest integer divisible by 3 and not larger than 10 is 9.
+
+def maxMultiple(divisor, bound):
+    count = 0
+    for i in range(1,bound+1):
+        if(i%divisor==0):
+            count = i
+
+    return count
+# print(maxMultiple(3,9)) # answer 9
+# print(maxMultiple(2,11)) # answer 10
+# ---voted most efficient----
+# def maxMultiple(divisor, bound):
+#     return bound - (bound % divisor)
+
+
+# ---Circle of Nubmers ---
+# Consider integer numbers from 0 to n - 1 written down along the circle in such a way 
+# that the distance between any two neighboring numbers is equal 
+# (note that 0 and n - 1 are neighboring, too).
+
+# Given n and firstNumber, find the number which is written in the radially opposite position 
+# to firstNumber.
+
+# Example : 
+    # For n = 10 and firstNumber = 2, the output should be
+    # circleOfNumbers(n, firstNumber) = 7.
+
+# [input] integer n
+    # A positive even integer.
+
+    # Guaranteed constraints:
+    # 4 ≤ n ≤ 20.
+def circleOfNumbers(n, firstNumber):
+    
+    if(firstNumber < (n/2)):
+        return (n/2)+firstNumber
+    elif(firstNumber > (n/2)):
+        return firstNumber - (n/2)
+    else:
+        return 0
+
+# ---Late Ride---
+# One night you go for a ride on your motorcycle. 
+# At 00:00 you start your engine, and the built-in timer automatically begins 
+# counting the length of your ride, in minutes. Off you go to explore the neighborhood.
+
+# When you finally decide to head back, you realize there's a chance 
+# the bridges on your route home are up, leaving you stranded! 
+# Unfortunately, you don't have your watch on you and don't know 
+# what time it is. All you know thanks to the bike's timer is 
+# that n minutes have passed since 00:00.
+
+# Using the bike's timer, calculate the current time. 
+# Return an answer as the sum of digits that the digital 
+# timer in the format hh:mm would show.
+
+def lateRide(n):
+    
+    diget = 0
+    hrs = n//60
+    diget += hrs//10
+    diget += hrs%10
+    mins = (n-(hrs*60))
+    diget += mins//10
+    diget += mins%10
+
+    return diget
+
+# print(lateRide(808))
+# --voted most efficent--
+# def lateRide(n):
+#     return sum(map(int, str(n // 60 * 100 + n % 60)))
+
+def lateRideClock(n):
+    s = ""
+    
+    hrs = n//60
+    s += str(hrs//10)
+    s += str(hrs%10)
+    s += ":"
+    mins = (n-(hrs*60))
+    s += str(mins//10)
+    s += str(mins%10)
+    return s
+
+
+# ---Phone Call---
+# Some phone usage rate may be described as follows:
+
+# first minute of a call costs min1 cents,
+# each minute from the 2nd up to 10th (inclusive) costs min2_10 cents
+# each minute after 10th costs min11 cents.
+# You have s cents on your account before the call. What is the duration of the longest call (in minutes rounded down to the nearest integer) you can have?
+
+# Example
+
+# For min1 = 3, min2_10 = 1, min11 = 2, and s = 20, the output should be
+# phoneCall(min1, min2_10, min11, s) = 14.
+
+# Here's why:
+
+# the first minute costs 3 cents, which leaves you with 20 - 3 = 17 cents;
+# the total cost of minutes 2 through 10 is 1 * 9 = 9, so you can talk 9 more minutes and still have 17 - 9 = 8 cents;
+# each next minute costs 2 cents, which means that you can talk 8 / 2 = 4 more minutes.
+# Thus, the longest call you can make is 1 + 9 + 4 = 14 minutes long.
+
+# Input/Output
+
+# [execution time limit] 4 seconds (py3)
+
+# [input] integer min1
+
+# Guaranteed constraints:
+# 1 ≤ min1 ≤ 10.
+
+# [input] integer min2_10
+
+# Guaranteed constraints:
+# 1 ≤ min2_10 ≤ 10.
+
+# [input] integer min11
+
+# Guaranteed constraints:
+# 1 ≤ min11 ≤ 10.
+
+# [input] integer s
+
+# Guaranteed constraints:
+# 2 ≤ s ≤ 500.
+
+# [output] integer
+def phoneCall(min1, min2_10, min11, s):
+    total = 0 
+
+    if s >= min1:
+        total += 1
+        s -= min1
+    while total < 10 and s > min2_10:
+        total += 1
+        s -= min2_10
+    while s > 0 and s >= min11 and total >= 10:
+        total += 1
+        s -= min11
+
+    return total
+
+# print(phoneCall(3,1,2,20)) # outputs 14
+# print(phoneCall(1,2,1,6)) # outputs 3
