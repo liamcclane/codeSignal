fb487d83dcc4cfe718b830887bf35edaeddc9125
liamcclane <lia_mcclane@msn.com>
2020-07-02 23:43:44 -0700

    daily algo

--- theArcade/Intro/9darkWilderness/knapsackLight.js
+++ theArcade/Intro/9darkWilderness/knapsackLight.js
@@ -15,12 +15,23 @@
  */
 function knapsackLight(value1, weight1, value2, weight2, maxW) {
 
-    if (maxW <= weight1 + weight2) { return value1 + value2 }
+    if (weight2 > maxW && weight1 > maxW) { return 0 }
 
-    if (value2 > value1 && weight2 < maxW) { return value2 }
+    if (maxW >= (weight1 + weight2)) { return value1 + value2 }
 
-    if (value1 > value2 && weight1 < maxW) { return value1 }
+    if (weight2 > maxW && weight1 <= maxW) { return value1 }
+
+    if (weight1 > maxW && weight2 <= maxW) { return value2 }
+
+    if (value2 > value1 && weight2 <= maxW) { return value2 }
+
+    if (value1 > value2 && weight1 <= maxW) { return value1 }
+
+    if (value1 === value1) { return weight2 < weight1 ? value2 : value1 }
+
+    return 0
 }
+
 
 
 /**Examples
