348a0b6fa146c6b352d744785446ef4d9bc5e4f3
liamcclane <lia_mcclane@msn.com>
2020-10-24 00:02:15 -0700

    chosen daily algo

---
+++ theArcade/Intro/10erputionOfLight/buildPalendrome.js
@@ -0,0 +1,12 @@
+/**buildPalendrome
+ * Given a string, find the shortest possible string which can 
+ * be achieved by adding characters to the end of 
+ * initial string to make it a palindrome.
+ * 
+ * ExampleFor st = "abcdc", the output 
+ * should bebuildPalindrome(st) = "abcdcba".
+ */
+function buildPalindrome(st) {
+
+}
+
