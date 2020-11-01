434ef0d406268a99c664025fd135dd9fa95d99bf
liamcclane <lia_mcclane@msn.com>
2020-10-31 22:06:04 -0700

    daily algo

---
+++ theArcade/Intro/12landOfLogic/sumUpNumbers.js
@@ -0,0 +1,39 @@
+/**
+ * CodeMaster has just returned from shopping. 
+ * He scanned the check of the items he bought 
+ * and gave the resulting string to Ratiorg to 
+ * figure out the total number of purchased items. 
+ * Since Ratiorg is a bot he is definitely going 
+ * to automate it, so he needs a program that 
+ * sums up all the numbers which appear in 
+ * the given input.
+ * 
+ * Help Ratiorg by writing a function that returns 
+ * the sum of numbers that appear in the given inputString.
+ * 
+ * Example
+ * 
+ * For inputString = "2 apples, 12 oranges", the output should be
+ * sumUpNumbers(inputString) = 14.
+ */
+function sumUpNumbers(inputString) {
+
+    let running = "", arrNums = [];
+
+    for (let i = 0; i < inputString.length; i++) {
+        let letter = inputString[i];
+        if(letter.match('[0-9]')){
+            running += letter;
+            if(i == inputString.length - 1) {
+                arrNums.push(running);
+            }
+        } else {
+            if(running != "") {
+                arrNums.push(running);
+            }
+            running = "";
+        }
+    }
+    return arrNums.map(ele => parseInt(ele)).reduce((accum, ele) => accum + ele, 0);
+}
+console.log(sumUpNumbers("2 apples, 12 oranges"));
